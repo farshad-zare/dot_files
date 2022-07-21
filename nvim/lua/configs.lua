6ebfc94aad9e17dddde516fe6d367aac93c83b12
Farshad Zare <farshad.z@outlook.com>
2022-07-21 19:37:51 +0430

    improved neovim theme

--- nvim/lua/configs.lua
+++ nvim/lua/configs.lua
@@ -75,10 +75,10 @@ vim.diagnostic.config({
 -- -----------------Highlight line number instead of having icons in sign column-------------------------------------------
 -- ------------------------------------------------------------------------------------------------------------------------
 vim.cmd [[
-    highlight! DiagnosticLineNrError guibg=#51202A guifg=#FF0000 gui=bold
-    highlight! DiagnosticLineNrWarn guibg=#51412A guifg=#FFA500 gui=bold
-    highlight! DiagnosticLineNrInfo guibg=#1E535D guifg=#00FFFF gui=bold
-    highlight! DiagnosticLineNrHint guibg=#1E205D guifg=#0000FF gui=bold
+    highlight! DiagnosticLineNrError guifg=#f70202 gui=bold
+    highlight! DiagnosticLineNrWarn guifg=#f59842 gui=bold
+    highlight! DiagnosticLineNrHint guifg=#42f545 gui=bold
+    highlight! DiagnosticLineNrInfo guifg=#4290f5 gui=bold
     
     sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=DiagnosticLineNrError
     sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=DiagnosticLineNrWarn
