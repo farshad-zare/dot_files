e8d479879f1443e3d29bc0df485ab9c51ff57194
Farshad Zare <farshad.z@outlook.com>
2023-06-11 12:29:45 +0330

    changed default size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,11 @@ return {
         harfbuzz_features = {"ss05", "ss03"}
     }),
     font_size = 11,
-    hide_tab_bar_if_only_one_tab = true,
+    initial_rows = 25,
+    initial_cols = 130,
+    term = 'wezterm',
+    window_decorations = "TITLE | RESIZE",
+    hide_tab_bar_if_only_one_tab = false,
     -- color_scheme = "Ollie"
     -- color_scheme = "Rippedcasts"
     color_scheme = "Thayer Bright"
